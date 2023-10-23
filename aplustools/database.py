402c62529a7285c109c7161d6d7c9183b857406b
Adalfarus <super_sniper_2227@gmx.net>
2023-10-23 16:27:39 +0200

    Little bugfixes in database and webtools

--- aplustools/database.py
+++ aplustools/database.py
@@ -16,13 +16,27 @@ class DBManager:
     def update_info(self, info: list, table: str, columns: list):
         if len(info) != len(columns):
             raise ValueError("Length of info must match the number of columns.")
+    
+        # Assuming first column is a unique identifier like ID
+        query_check = f"SELECT COUNT(*) FROM {table} WHERE {columns[0]} = ?"
+        self.cursor.execute(query_check, (info[0],))
+        exists = self.cursor.fetchone()[0]
 
-        query = f"INSERT INTO {table} ({', '.join(columns)}) VALUES ({', '.join('?' for _ in info)})"
-        try:
-            self.cursor.execute(query, info)
-            self.conn.commit()
-        except Exception as e:
-            print(f"Error updating info: {e}")
+        if exists:
+            placeholders = ', '.join([f"{column} = ?" for column in columns])
+            query = f"UPDATE {table} SET {placeholders} WHERE {columns[0]} = ?"
+            try:
+                self.cursor.execute(query, (*info, info[0]))
+                self.conn.commit()
+            except Exception as e:
+                print(f"Error updating info: {e}")
+        else:
+            query = f"INSERT INTO {table} ({', '.join(columns)}) VALUES ({', '.join('?' for _ in info)})"
+            try:
+                self.cursor.execute(query, info)
+                self.conn.commit()
+            except Exception as e:
+                print(f"Error updating info: {e}")
 
     def get_info(self, table: str, columns: list) -> list:
         query = f"SELECT {', '.join(columns)} FROM {table}"
