cce1966e5bea4de2770731101e18221bbf1d0c33
Adalfarus <super_sniper_2227@gmx.net>
2023-11-22 21:46:03 +0100

    Small bug fixes

--- aplustools/logs.py
+++ aplustools/logs.py
@@ -59,7 +59,7 @@ class Logger(object):
     def __enter__(self):
         return self
     
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
     def __del__(self):
