fe9b3a9b26a254c552d951fb2a4bd3abd399a94b
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-01-12 12:49:02 +0100

    Updated main __init__.py to not import everything

--- aplustools/__init__.py
+++ aplustools/__init__.py
@@ -1,5 +1,5 @@
 # aplustools
-from .io import loggers, environment # renamed from logging to logs to avoid naming conflict with urllib
-from .data import updaters, database
-from .utils import genpass, imagetools
-from .web import webtools
+#from .io import loggers, environment # renamed from logging to logs to avoid naming conflict with urllib
+#from .data import updaters, database
+#from .utils import genpass, imagetools
+#from .web import webtools
