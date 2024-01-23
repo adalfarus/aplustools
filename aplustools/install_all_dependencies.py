64ac849813f31730a1888ddcc6a80a9a8a6499e3
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-01-23 12:39:59 +0100

    Update install_all_dependencies.py

--- aplustools/install_all_dependencies.py
+++ aplustools/install_all_dependencies.py
@@ -6,6 +6,13 @@ def execute_python_command(arguments: list=None, run: bool=False, *args, **kwarg
     if run: arguments.append("--run") # Added to remain consistent with executing in same python environment
     return subprocess.run([sys.executable] + arguments, *args, **kwargs)
     
-for dep in []:
-    execute_python_command("pip", "install", dep)
+for dep in ["requests==2.31.0", 
+            "Pillow==10.1.0", 
+            "bs4==4.12.2", 
+            "datetime==5.2", 
+            "duckduckgo_search==3.9.3", 
+            "rich==13.7.0", 
+            "pycryptodome==3.19.0"]
+    execute_python_command(arguments=
+               ["pip", "install", dep])
     
