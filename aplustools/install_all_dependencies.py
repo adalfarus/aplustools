388368b61d44baa538513edaec2cd21c2ac14d50
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-01-23 12:37:14 +0100

    Update install_all_dependencies.py

--- aplustools/install_all_dependencies.py
+++ aplustools/install_all_dependencies.py
@@ -1,1 +1,11 @@
 print("placeholder")
+
+def execute_python_command(arguments: list=None, run: bool=False, *args, **kwargs) -> CompletedProcess[str]:
+    if arguments == None: arguments = []
+    log(' '.join([sys.executable] + arguments), LogType.DEBUG)
+    if run: arguments.append("--run") # Added to remain consistent with executing in same python environment
+    return subprocess.run([sys.executable] + arguments, *args, **kwargs)
+    
+for dep in []:
+    execute_python_command("pip", "install", dep)
+    
