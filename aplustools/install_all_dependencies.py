81ae12c67a072be6df0c08976121778e43ae4492
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-01-23 12:47:52 +0100

    Update install_all_dependencies.py
    Last fixes

--- aplustools/install_all_dependencies.py
+++ aplustools/install_all_dependencies.py
@@ -1,9 +1,10 @@
 import subprocess
+import sys
 
-def execute_python_command(arguments: list=None, run: bool=False, *args, **kwargs) -> subprocess.CompletedProcess[str]:
+def execute_python_command(arguments: list=None, *args, **kwargs) -> subprocess.CompletedProcess[str]:
     if arguments == None: arguments = []
-    log(' '.join([sys.executable] + arguments), LogType.DEBUG)
-    if run: arguments.append("--run") # Added to remain consistent with executing in same python environment
+    print(' '.join([sys.executable] + arguments), LogType.DEBUG)
+    # Added to remain consistent with executing in same python environment
     return subprocess.run([sys.executable] + arguments, *args, **kwargs)
     
 for dep in ["requests==2.31.0", 
@@ -13,8 +14,11 @@ for dep in ["requests==2.31.0",
             "duckduckgo_search==3.9.3", 
             "rich==13.7.0", 
             "pycryptodome==3.19.0"]:
-    execute_python_command(arguments=
-               ["pip", "install", dep])
+   try:
+      execute_python_command(arguments=
+                  ["pip", "install", dep])
+   except Exception as e:
+      print("An error occurred:" + e)
                
 input("Done, press ENTER to exit ...")
     
