53f43d799f078c3c562a1b600d20a9e73aca3c3b
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-01-27 18:53:21 +0100

    Update direct_functions.py

--- aplustools/utils/direct_functions.py
+++ aplustools/utils/direct_functions.py
@@ -9,7 +9,7 @@ def install_dependencies():
         return subprocess.run([sys.executable] + arguments, *args, **kwargs)
     for dep in ["Pillow==10.2.0",
                 "rich==13.7.0",
-                "pycryptodome==3.19.0"]:
+                "pycryptodome==3.20.0"]:
        try:
           proc = execute_python_command(arguments=
                          ["-m", "pip", "install", dep])
