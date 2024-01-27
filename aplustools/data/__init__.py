b68626dec760b2dc099438e3c7a0f6b4baab9b52
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-01-27 12:00:57 +0100

    Changes required for v0.1.4.2 release

--- aplustools/data/__init__.py
+++ aplustools/data/__init__.py
@@ -1,2 +1,36 @@
-# data
-#from . import database, updaters
+# data __init__
+
+class _LazyModuleLoader:
+    def __init__(self, module_name):
+        self.module_name = module_name
+        self.module = None
+
+    def _load_module(self):
+        if self.module is None:
+            try:
+                self.module = __import__(self.module_name, globals(), locals(), ['*'])
+            except ImportError:
+                raise ImportError("Optional module not installed. Please install it to use this feature.")
+        return self.module
+
+    def __repr__(self):
+        if self.module is None:
+            return f"<LazyModuleLoader for '{self.module_name}'>"
+        else:
+            return repr(self.module)
+
+    def __getattr__(self, item):
+        module = self._load_module()
+        return getattr(module, item)
+
+    def __dir__(self):
+        self._load_module()
+        return dir(self.module)
+
+
+# Lazy loading modules
+database = _LazyModuleLoader('aplustools.data.database')
+updaters = _LazyModuleLoader('aplustools.data.updaters')
+
+# Define __all__ to limit what gets imported with 'from <package> import *'
+__all__ = ['database', 'updaters']
--- aplustools/io/__init__.py
+++ aplustools/io/__init__.py
@@ -1,2 +1,36 @@
-# io
-#from . import environment, loggers
+# io __init__
+
+class _LazyModuleLoader:
+    def __init__(self, module_name):
+        self.module_name = module_name
+        self.module = None
+
+    def _load_module(self):
+        if self.module is None:
+            try:
+                self.module = __import__(self.module_name, globals(), locals(), ['*'])
+            except ImportError:
+                raise ImportError("Optional module not installed. Please install it to use this feature.")
+        return self.module
+
+    def __repr__(self):
+        if self.module is None:
+            return f"<LazyModuleLoader for '{self.module_name}'>"
+        else:
+            return repr(self.module)
+
+    def __getattr__(self, item):
+        module = self._load_module()
+        return getattr(module, item)
+
+    def __dir__(self):
+        self._load_module()
+        return dir(self.module)
+
+
+# Lazy loading modules
+environment = _LazyModuleLoader('aplustools.io.environment')
+loggers = _LazyModuleLoader('aplustools.io.loggers')
+
+# Define __all__ to limit what gets imported with 'from <package> import *'
+__all__ = ['environment', 'loggers']
