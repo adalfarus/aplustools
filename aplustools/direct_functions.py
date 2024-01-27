b9d6d05cadd362bdbe7c0010336b3aa8db2a50be
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-01-27 18:01:18 +0100

    Update direct_functions.py

--- aplustools/direct_functions.py
+++ aplustools/direct_functions.py
@@ -1,17 +1,23 @@
 import subprocess
 import sys
+import os
+import warnings
 
-def execute_python_command(arguments: list=None, *args, **kwargs) -> subprocess.CompletedProcess[str]:
-    if arguments == None: arguments = []
+
+def execute_python_command(arguments: list = None, *args, **kwargs) -> subprocess.CompletedProcess[str]:
+    if arguments is None:
+        arguments = []
     print(' '.join([sys.executable] + arguments))
     # Added to remain consistent with executing in same python environment
     return subprocess.run([sys.executable] + arguments, *args, **kwargs)
+
 
 def interruptCTRL():
     """
     Simulates a user CTRL+C exit.
     """
-    exit(-1073741510)
+    exit(-1073741510)  # 130
+
 
 def install_all_dependencies():
     for dep in ["requests==2.31.0",
@@ -22,13 +28,33 @@ def install_all_dependencies():
                 "rich==13.7.0",
                 "pycryptodome==3.19.0",
                 "PySide6==6.5.1.1"]:
-       try:
-          proc = execute_python_command(arguments=
-                         ["-m", "pip", "install", dep])
-          if proc.returncode != 0:
-             raise
-       except Exception as e:
-          print("An error occurred:" + str(e))
+        try:
+            proc = execute_python_command(arguments=
+                                          ["-m", "pip", "install", dep])
+            if proc.returncode != 0:
+                raise
+        except Exception as e:
+            print("An error occurred:" + str(e))
 
     print("Done, all possible dependencies installed ...")
-    
+
+
+def set_dir_to_ex():
+    import __main__
+    # Get the directory where the main script (or frozen exe) is located
+    if getattr(sys, 'frozen', False):
+        # If the script is running as a bundled executable created by e.g. PyInstaller
+        main_dir = os.path.dirname(sys.executable)
+    else:
+        # If the script is running as a normal Python script
+        if hasattr(__main__, '__file__'):
+            main_dir = os.path.dirname(os.path.abspath(__main__.__file__))
+        else:
+            main_dir = os.path.dirname(os.getcwd())
+            warnings.warn(
+                "Could not set the current working directory to the location of the main script or executable",
+                RuntimeWarning,
+                stacklevel=2
+            )
+    # Change the current working directory to the main script directory
+    os.chdir(main_dir)
