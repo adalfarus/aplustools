5b34beea74c778e1d60eeb6822f29f1b4cf13fd1
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-03-23 12:29:18 +0100

    Update test_general.py

--- aplustools/tests/test_general.py
+++ aplustools/tests/test_general.py
@@ -1,12 +1,12 @@
-from aplustools import _direct_functions
+from aplustools import execute_python_command, set_dir_to_ex
 from aplustools.web import web_request
 
 
 class TestDirectFunctions:
     def test_all(self):
         try:
-            # _direct_functions.set_dir_to_ex()  # Won't work
-            _direct_functions.execute_python_command(["--version"])
+            # set_dir_to_ex()  # Won't work
+            execute_python_command(["--version"])
         except Exception as e:
             print(f"Exception occurred {e}")
             assert False
