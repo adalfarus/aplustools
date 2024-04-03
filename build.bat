1cde1bae0fe270a361dcaf4ec8cbe6a733d3a47d
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-04-03 11:45:42 +0200

    Added readme and polished up the genpass sub-module

--- build.bat
+++ build.bat
@@ -1,6 +1,13 @@
 @echo off
 py -m build
 
-if "%cmdcmdline%"=="" (
-  echo Finished ...
+REM https://superuser.com/questions/1301373/how-can-i-tell-whether-a-batch-file-was-run-from-a-command-window
+if "%CMDCMDLINE:"=%" == "%COMSPEC% " (
+  pause
+) else if "%CMDCMDLINE%" == "cmd.exe" (
+  pause
+) else if "%CMDCMDLINE:"=%" == "%COMSPEC% /c %~dpf0 " (
+  REM started in command window
+) else (
+  REM started from other batch file
 )
