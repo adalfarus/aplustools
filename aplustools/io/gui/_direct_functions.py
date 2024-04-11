3b7096786be9c9389301f901e6379eb83c10d750
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-04-11 20:11:28 +0200

    Added io.gui

---
+++ aplustools/io/gui/_direct_functions.py
@@ -0,0 +1,8 @@
+from aplustools.package import install_dependencies_lst as _install_dependencies_lst
+
+
+def install_dependencies():
+    success = _install_dependencies_lst(["PySide6==6.6.1"])
+    if not success:
+        return
+    print("Done, all possible dependencies for the data module installed ...")
