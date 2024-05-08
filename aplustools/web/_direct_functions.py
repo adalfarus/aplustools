5317740f7ee3db6fcd12cc29a2740e2f8d886558
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-05-08 17:50:55 +0200

    Update _direct_functions.py

--- aplustools/web/_direct_functions.py
+++ aplustools/web/_direct_functions.py
@@ -3,7 +3,7 @@ from aplustools.package import install_dependencies_lst as _install_dependencies
 
 def install_dependencies():
     success = _install_dependencies_lst(["requests==2.31.0", "BeautifulSoup4==4.12.3", "duckduckgo_search==3.9.3",
-                                         "aiohttp==3.9.3"])
+                                         "aiohttp==3.9.4"])
     if not success:
         return
     print("Done, all possible dependencies for the data module installed ...")
