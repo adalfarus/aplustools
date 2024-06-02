48785a2d461557118cf0ad155a6a5f8d38464611
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-06-02 18:00:28 +0200

    Fixed dependency

--- aplustools/web/_direct_functions.py
+++ aplustools/web/_direct_functions.py
@@ -2,7 +2,7 @@ from aplustools.package import install_dependencies_lst as _install_dependencies
 
 
 def install_dependencies():
-    success = _install_dependencies_lst(["requests==2.31.0", "BeautifulSoup4==4.12.3", "aiohttp==3.9.4"])
+    success = _install_dependencies_lst(["requests==2.32.0", "BeautifulSoup4==4.12.3", "aiohttp==3.9.4"])
     if not success:
         return
     print("Done, all possible dependencies for the data module installed ...")
