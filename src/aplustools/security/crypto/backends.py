80b0bc9c62e7f934b7467cb5f004d54b66ec53b0
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-06-28 12:33:25 +0200

    Update backends.py

--- src/aplustools/security/crypto/backends.py
+++ src/aplustools/security/crypto/backends.py
@@ -4,4 +4,4 @@ from ...data import enum_auto
 class MainBackend:
     """One of the cryptography backends"""
     cryptography = enum_auto()
-    PyCryptodom = enum_auto()
+    pycryptodomex = enum_auto()
