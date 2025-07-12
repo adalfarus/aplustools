82011b201b207ea59987c863a000d604178e0a2e
linnie <cariel.becker@gmx.de>
2025-07-12 17:22:26 +0200

    Removed scrapped dir

--- scrapped/cert/__init__.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-from ._certificate import Certificate, CertificateEncoding
-from ._ca import CertificateAuthority, Key, PublicKey
-from ._name import Name
-
-__all__ = ["Certificate", "CertificateAuthority", "Name", "CertificateEncoding", "Key", "PublicKey"]
